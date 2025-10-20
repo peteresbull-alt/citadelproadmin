ca935f3e73cd47a71feca4e30d63b59a3f53cbeb
ghostpete <ghostcoderme@gmail.com>
2025-10-20 10:50:39 +0100

    updated deployment files

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-# Update pip
-echo "Updating pip..."
-python3  pip install -U pip
+echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
 
-# Install dependencies
-
-echo "Installing project dependencies..."
-python3  -m pip install -r requirements.txt
-
-# Make migrations
-echo "Making migrations..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-# Collect staticfiles
-echo "Collect static..."
+echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
 
-echo "Build process completed!"
+echo "Build process completed successfully!"
+
+
+# # Update pip
+# echo "Updating pip..."
+# python3 -m pip install -U pip
+# python3 -m pip install --upgrade pip
+
+# # Install dependencies
+
+# echo "Installing project dependencies..."
+# python3  -m pip install -r requirements.txt
+
+# # Make migrations
+# # echo "Making migrations..."
+# # python3 manage.py makemigrations --noinput
+# # python3 manage.py migrate --noinput
+
+# # Collect staticfiles
+# echo "Collect static..."
+# python3 manage.py collectstatic --noinput --clear
+
+# echo "Build process completed!"
 
