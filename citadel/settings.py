1da43cabe335c96c1b14291b1adea68515336271
ghostpete <ghostcoderme@gmail.com>
2025-11-02 08:17:30 +0100

    final addition

--- citadel/settings.py
+++ citadel/settings.py
@@ -104,19 +104,19 @@ TEMPLATES = [
 # ----------------------------
 # DATABASE
 # ----------------------------
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # ----------------------------
 # STATIC & MEDIA
